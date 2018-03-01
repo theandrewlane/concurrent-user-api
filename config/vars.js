4c3737e2df8b1709ae1d87445e110719df44512b
theandrewlane <theeandrewlane@gmail.com>
2018-02-28 22:49:58 -0700

    updating db name

--- config/vars.js
+++ config/vars.js
@@ -1,5 +1,5 @@
 module.exports = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3000,
-  mongo: process.env.MONGO_URI || 'mongodb://localhost:27017/node-starter'
+  mongo: process.env.MONGO_URI || 'mongodb://localhost:27017/uic-operator-api'
 };
